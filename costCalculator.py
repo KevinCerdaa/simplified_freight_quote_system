4472e607e3b1de2f2649c4ef79965c5c6db2f5c5
KevinCerdaa <kevfranew@gmail.com>
2026-09-21 19:35:18 -0600

    First version of cost code

---
+++ costCalculator.py
@@ -0,0 +1,14 @@
+BASE_EFFICIENCY = 2.8
+PENALTY_PER_TON = 0.03
+DIESEL_PRICE = 27.01
+AVERAGE_SPEED = 65
+
+def get_fuel_efficiency(weight):
+    return BASE_EFFICIENCY - (weight * PENALTY_PER_TON)
+
+def get_fuel(kilometers, weight):
+    return kilometers / get_fuel_efficiency(weight)
+
+def getFuelCost (kilometers, weight):
+    return get_fuel(kilometers, weight) * DIESEL_PRICE
+
